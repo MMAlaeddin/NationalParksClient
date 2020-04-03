5085373166dedcfddf7fdbf856e1f12f7b592b63
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-03 12:20:01 -0700

    add park and apihelper model structure

---
+++ NationalParksClient/Models/Park.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+  
+  namespace NationalParksClient.Models
+  {
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Description { get; set; }
+    public int ClimbingRoutes { get; set; }
+    public int Campgrounds { get; set; }
+    public int GeneralStores { get; set; }
+
+    public static List<Park> GetParks()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Park> parkList = JsonConvert.DeserializeObject<List<Park>>(jsonResponse.ToString());
+
+      return parkList;
+    }
+  }
+}
